d7cc97e76738bfe74d94f519af08f0591f929111
AHolloway <AHolloway@L11713038597.wctc.msft>
2012-09-26 18:32:40 -0500

    First Commit.  Made some changes to lab1 already and copied solution to dip.lab2.solution folder.

---
+++ src/dip/lab1/student/solution1/EmployeeStrategy.java
@@ -0,0 +1,10 @@
+
+package dip.lab1.student.solution1;
+
+/**
+ *
+ * @author AHolloway
+ */
+public interface EmployeeStrategy {
+    public abstract double getAnnualWages();
+}
---
+++ src/dip/lab1/student/solution1/HRService.java
@@ -0,0 +1,46 @@
+package dip.lab1.student.solution1;
+
+import javax.swing.JOptionPane;
+
+/**
+ * A high-level class that delegates to employee objects to do the work. Does
+ * this class design follow the DIP? If not, fix it.
+ *
+ * @author your name goes here
+ */
+public class HRService {
+    /** 
+     * Constants can be used for retrieving annual wages of
+     * various employee types. Is this the best way to go? Do we have
+     * type safety?
+     */
+    
+
+    /**
+     * Alternatively to the above, we could use an enum. Does this improve
+     * type safety?
+     */
+   
+
+   
+    /**
+     * This is a truly horrible way to make decisions. Very rigid and fragile!
+     * @param e - an employee object (not truly polymorphic!)
+     * employeeType - a String identifying the employee type. A constant
+     * (see above) should be used for this, but can you guarantee it will be?
+     * Are there other options? Hint: enum
+     * @return the annual compensation for supported employee types
+     */
+   
+
+
+    /**
+     * This is another truly horrible way to make decisions.
+     * Very rigid and fragile!
+     * @param e - an employee object (not truly polymorphic!)
+     * @return the annual compensation for supported employee types
+     */
+    public double getAnnualCompensationForEmployee(EmployeeStrategy e) {
+        return e.getAnnualWages();
+    }
+}
