7fbc7f28381ead2a2753588041fdfb599fd4a003
aholloway <aholloway@L11713038597.wctc.msft>
2012-10-03 17:03:14 -0500

    minor changes to Startup class for lab3.

--- src/dip/lab3/Startup.java
+++ src/dip/lab3/Startup.java
@@ -5,27 +5,29 @@ package dip.lab3;
  * @Drew Holloway
  */
 public class Startup {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         //instantiate low level classes
-        TipOfTheDayMessageProvider tipOfTheDay= new TipOfTheDayMessageProvider();
-        
-        ConsoleRenderer cr= new ConsoleRenderer();
-        
+        TipOfTheDayMessageProvider tipOfTheDay = new TipOfTheDayMessageProvider();
+
+        ConsoleRenderer cr = new ConsoleRenderer();
+
         //create higher level class (service class) passing in the low level
         // classes as parameters
-        MessagingService messagingService=new MessagingService(tipOfTheDay,cr);
+        MessagingService messagingService = new MessagingService(tipOfTheDay, cr);
         messagingService.renderMessage();
-        
+
         //demonstrate capability to switch both the message provider
         // and the message renderer at runtime
         messagingService.setMessageProvider(new ErrorMessageProvider());
         messagingService.setMessageRenderer(new GuiRenderer());
-        
+
         messagingService.renderMessage();
-        
+
         messagingService.setMessageProvider(new RandomMessageProvider());
-        messagingService.renderMessage();
-        messagingService.renderMessage();
-        messagingService.renderMessage();
+
+        for (int i = 1; i <= 6; i++) {
+            messagingService.renderMessage();
+        }
     }
 }
